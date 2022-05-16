b7ff0ad4ad3d7381debd19c28ca22e6ceb3dc8a8
Eugenio <culurciello@gmail.com>
2022-05-16 12:40:18 -0400

    sorted match_text

--- text_similarity.py
+++ text_similarity.py
@@ -1,11 +1,16 @@
 # text similarity
 # inspired by: https://newscatcherapi.com/blog/ultimate-guide-to-text-similarity-with-python
 
+print('Loading SentenceTransformer...')
+
 import sys
+import numpy as np
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('stsb-roberta-large')
 
-text_options = [
+print('Done!')
+
+commands_all = [
     "plot the country versus the population",
     "plot countries versus population",
     "plot country vs population",
@@ -16,25 +21,36 @@ text_options = [
 ]
 
 
-def match_text(text_options, text_to_compare):
-    embeddings = model.encode(text_options, convert_to_tensor=True)
-    embedinput = model.encode(text_to_compare, convert_to_tensor=True)
+def match_text(text_commands, input_command):
+    print('Embedding all possible commands...')
+    embeddings = model.encode(commands_all, convert_to_tensor=True)
+    print('Embedding input commands')
+    embedinput = model.encode(input_command, convert_to_tensor=True)
+    print('Done!')
 
     similarity = []
-    for i, line in enumerate(text_options):
-        # print(line, text_to_compare)
+    for i, line in enumerate(commands_all):
+        # print(line, input_command)
         s = util.pytorch_cos_sim(embeddings[i], embedinput).item()
         similarity.append(s)
-  
-    print(similarity) 
 
+    similarity = np.sort(similarity)[::-1]
+    sorted_idx = np.argsort(similarity)[::-1]
+  
+    return similarity, sorted_idx
 
 
 if __name__ == '__main__':
 
     if len(sys.argv)>1:
-        text_to_compare = sys.argv[1] # TEST
+        input_command = sys.argv[1] # TEST
     else:
-        text_to_compare = "plot country versus population"
+        input_command = "plot country versus population"
 
-    match_text(text_options, text_to_compare)
+
+    print('You are comparing: ', input_command, ' to all possible commands!')
+
+    similarity, sorted_idx = match_text(commands_all, input_command)
+
+    for i,value in enumerate(similarity):
+        print(commands_all[sorted_idx[i]], ' -- ', similarity[i])
