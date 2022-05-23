1f3cb778e5fadcf834c826528a58844079a1e9ac
Eugenio <culurciello@gmail.com>
2022-05-23 13:59:25 -0400

    cleanup

--- app.py
+++ app.py
@@ -25,28 +25,29 @@ with open(dataset_file, 'r') as infile:
 
 
 def plot(a, b, limit=10, figsize=(8, 6), dpi=120, fontsize=12):
-    # plot a versus b - finding ab,b in dataset
+    # plot a versus b - finding a,b in dataset
     # find match in a:
     ai = [i for i, s in enumerate(dataset[0]) if a.lower() in s.lower()]
     # find match in b:
     bi = [i for i, s in enumerate(dataset[0]) if b.lower() in s.lower()]
     data = np.array(dataset)
-    # Generate the figure **without using pyplot**.
+    # create figure
     fig = Figure(figsize=figsize, dpi=dpi)
     ax = fig.subplots()
     ax.set_title(a+" vs "+b, fontsize=fontsize)
     ax.set_xlabel(a, fontsize=fontsize)
     ax.set_ylabel(b, fontsize=fontsize)
     ax.plot(data[1:limit, ai[0]], data[1:limit, bi[0]])
-    # save plot
+
+    # save plot to file
     # fig.savefig("test_figs.png", format="png") # uncomment this to save png file
-    # Save it to a temporary buffer.
+
+    # save plot to buffer / no file
     buf = BytesIO()
     fig.tight_layout()
     fig.savefig(buf, format="png")
-    # Embed the result in the html output.
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    # return f"<img src='data:image/png;base64,{data}'/>"
+    
     return data
 
 
--- test_plots.py
+++ test_plots.py
@@ -13,23 +13,24 @@ with open(dataset_file, 'r') as infile:
 
 
 def plot(a, b, limit=10, figsize=(8, 6), dpi=120, fontsize=12):
-    # plot a versus b - finding ab,b in dataset
+    # plot a versus b - finding a,b in dataset
     # find match in a:
     ai = [i for i, s in enumerate(dataset[0]) if a.lower() in s.lower()]
     # find match in b:
     bi = [i for i, s in enumerate(dataset[0]) if b.lower() in s.lower()]
     data = np.array(dataset)
-    # Generate the figure **without using pyplot**.
+    # create figure
     fig = Figure(figsize=figsize, dpi=dpi)
     ax = fig.subplots()
     ax.set_title(a+" vs "+b, fontsize=fontsize)
     ax.set_xlabel(a, fontsize=fontsize)
     ax.set_ylabel(b, fontsize=fontsize)
-    # ax.plot(dataset[0], dataset[1])
     ax.plot(data[1:limit, ai[0]], data[1:limit, bi[0]])
-    # save plot
+
+    # save plot to file
     fig.tight_layout()
     fig.savefig("test_figs.png", format="png")
+    
     return data
 
 
