9d347ba923326e9b3da5561ffb8724367acf02b0
Eugenio Culurciello <culurciello@gmail.com>
2022-09-26 10:41:07 -0400

    fixed sorting mistake

--- text_similarity.py
+++ text_similarity.py
@@ -33,11 +33,13 @@ def match_text(text_commands, input_command):
         # print(line, input_command)
         s = util.pytorch_cos_sim(embeddings[i], embedinput).item()
         similarity.append(s)
+        print(line, s)
 
-    similarity = np.sort(similarity)[::-1]
+    similarity_sorted = np.sort(similarity)[::-1]
     sorted_idx = np.argsort(similarity)[::-1]
-  
-    return similarity, sorted_idx
+    print(similarity_sorted, sorted_idx) 
+    
+    return similarity_sorted, sorted_idx
 
 
 if __name__ == '__main__':
@@ -48,9 +50,10 @@ if __name__ == '__main__':
         input_command = "plot country versus population"
 
 
-    print('You are comparing: ', input_command, ' to all possible commands!')
+    print('You are comparing: "'+ input_command + '" to all possible commands!')
 
     similarity, sorted_idx = match_text(commands_all, input_command)
 
+    print("Comparing results:")
     for i,value in enumerate(similarity):
         print(commands_all[sorted_idx[i]], ' -- ', similarity[i])
